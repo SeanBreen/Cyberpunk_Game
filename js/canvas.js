9664ef308df551e980aa0b5e72b8247a0895f672
Sean Breen <sean15008594@gmail.com>
2018-05-21 00:56:19 +0100

    Amended level functionality
    Added clearScreen function, moved level variables to their own file and created an event listener for mouse movement

--- js/canvas.js
+++ js/canvas.js
@@ -1,36 +1,25 @@
+//Canvas settings
 var canvas = document.getElementById("canvas");
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 var ctx = canvas.getContext("2d");
+var mouse = [0,0];
+canvas.addEventListener("mousemove", function(ev) {
+    mouse = [ev.clientX,ev.clientY];
+});
+//Tile size and position of the game grid on screen
 var tileSize = 80;
 var initialStartX = (window.innerWidth/2) - ((tileSize*10)/2);
 var initialStartY = (window.innerHeight/2) - ((tileSize*10)/2);
-var level = [
-  0,0,0,0,0,0,0,0,0,0,
-  0,0,0,0,0,0,0,0,0,0,
-  0,0,0,0,0,0,0,0,0,0,
-  0,0,0,0,0,0,0,0,0,0,
-  0,0,0,0,0,0,0,0,0,0,
-  0,0,0,0,0,0,0,0,0,0,
-  0,0,0,0,0,0,0,0,0,0,
-  0,0,0,0,0,0,0,0,0,0,
-  0,0,0,0,0,0,0,0,0,0,
-  0,0,0,0,0,0,0,0,0,0
-];
 
-var roads = [
-  0,0,0,0,0,0,0,0,0,0,
-  0,0,0,0,0,0,0,0,0,0,
-  0,0,0,0,0,0,0,0,0,0,
-  0,0,0,0,0,0,0,0,0,0,
-  0,0,0,0,0,0,0,0,0,0,
-  0,0,0,0,0,0,0,0,0,0,
-  0,0,0,0,0,0,0,0,0,0,
-  0,0,0,0,0,0,0,0,0,0,
-  0,0,0,0,0,0,0,0,0,0,
-  0,0,0,0,0,0,0,0,0,0
-];
 
+//Clear Screen
+function clearScreen() {
+  ctx.fillStyle = "#fff";
+  ctx.fillRect(0,0,window.innerWidth,window.innerHeight);
+}
+
+//Draw all of the tiles for the level background
 function drawLevel(level) {
   var startX = initialStartX;
   var startY = initialStartY;
@@ -46,13 +35,14 @@ function drawLevel(level) {
   }
 }
 
+//Draw the roads for the game
 function drawRoads(roads) {
-  var startX = 0;
-  var startY = 0;
+  var startX = initialStartX;
+  var startY = initialStartY;
   for (i=0;i<roads.length;i++) {
-    if (i%16 === 0 && i!=0) {
+    if (i%10 === 0 && i!=0) {
       startY+=tileSize;
-      startX = 0;
+      startX = initialStartX;
     }
     if (roads[i] !=0){
       var textureName = getTextureName(roads[i],"r");
@@ -64,15 +54,18 @@ function drawRoads(roads) {
   }
 }
 
+//Returns the texture name from a given integer and texture type (eg. road)
 function getTextureName(no,type) {
+  //Get the type of texture
   switch(type) {
     case "l":
+      //Get the number associated with the specific texture
       switch(no) {
         case 0:
           return "level/tile";
           break;
         case 1:
-          return "";
+          return "level/unlocked";
           break;
       }
       break;
@@ -89,5 +82,11 @@ function getTextureName(no,type) {
           break;
       }
       break;
+    case "c":
+      switch(no) {
+        case 1:
+          return "sprites/vehicles/tempCar";
+          break;
+      }
   }
 }
